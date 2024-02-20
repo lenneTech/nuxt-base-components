5c21a2c631230111250a05d555e7a1c96a8a7630
timdreute <tim.dreute@lenne.tech>
2024-02-20 13:37:11 +0100

    add composables and base components

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+const isExpanded = ref(false)
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <p class="text-red-500">Test</p>
+    <BaseButton size="md" color="primary" appearance="regular">
+      Regular
+    </BaseButton>
+    <BaseAccordion class="text-left" :expanded="isExpanded" @click="isExpanded = !isExpanded">
+      <template #title>
+        <p class="font-semibold"> Example</p>
+      </template>
+      <p class="whitespace-pre-wrap"> Example Content </p>
+    </BaseAccordion>
   </div>
 </template>
