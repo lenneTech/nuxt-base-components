e996c747224e2ce39cfc88ccf2c51dbae9b8d7b1
Pascal Klesse <pascal.klesse@lenne.tech>
2024-10-02 14:37:17 +0200

    feat: add config

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'dark',
+    brandTitle: 'Docs | lenne.Tech',
+    brandUrl: 'https://lenne.tech',
+    brandImage: 'https://lenne.tech/assets/images/logo-header.svg',
+    brandTarget: '_blank',
+});
