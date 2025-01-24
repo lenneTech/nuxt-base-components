604349084b8bdea7ad883895a946e95917d76b49
Daniel König <daniel.koenig@lenne.tech>
2025-01-24 15:09:08 +0100

    feat(TEC-27): fixed styling and added content to tailwind config

--- blocks/BlockPasswordSet.vue
+++ blocks/BlockPasswordSet.vue
@@ -37,32 +37,30 @@ const onSubmit = handleSubmit.withControlled(async (values) => {
 </script>
 
 <template>
-  <div class="flex min-h-full flex-1 flex-col justify-center">
+  <div class="flex min-h-dvh flex-1 flex-col justify-center">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
       <NuxtImg alt="Logo" class="mx-auto w-auto" src="https://placehold.co/100x20/png"/>
       <h2 class="mt-6 text-black text-2xl font-semibold text-center">Neues Passwort vergeben</h2>
     </div>
 
     <div class="mt-12 sm:mx-auto sm:w-full sm:max-w-[480px]">
-      <div class="shadow sm:rounded-lg">
-        <p>Das Passwort sollte eine Mindestlänge von 6 Zeichen haben.</p>
+      <p>Das Passwort sollte eine Mindestlänge von 6 Zeichen haben.</p>
 
-        <form novalidate @submit="onSubmit">
-          <div class="mt-6">
-            <FormPassword autocomplete="new-password" label="Neues Passwort eingeben" name="password"/>
-          </div>
-          <div class="mt-6">
-            <FormPassword autocomplete="new-password" label="Neues Passwort wiederholen" name="password_confirm"/>
-          </div>
+      <form novalidate @submit="onSubmit">
+        <div class="mt-6">
+          <FormPassword autocomplete="new-password" label="Neues Passwort eingeben" name="password"/>
+        </div>
+        <div class="mt-6">
+          <FormPassword autocomplete="new-password" label="Neues Passwort wiederholen" name="password_confirm"/>
+        </div>
 
-          <FormSubmit
-              :disabled="!meta.dirty"
-              :is-submitting="isSubmitting"
-              class="flex items-center justify-center mt-12"
-              label="Speichern"
-          />
-        </form>
-      </div>
+        <FormSubmit
+            :disabled="!meta.dirty"
+            :is-submitting="isSubmitting"
+            class="flex items-center justify-center mt-12"
+            label="Speichern"
+        />
+      </form>
     </div>
   </div>
 </template>
--- blocks/BlockRegister.vue
+++ blocks/BlockRegister.vue
@@ -38,86 +38,84 @@ const onSubmit = handleSubmit.withControlled(async (values) => {
 </script>
 
 <template>
-  <div class="flex min-h-full flex-1 flex-col justify-center">
+  <div class="flex min-h-dvh flex-1 flex-col justify-center">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
       <NuxtImg alt="Logo" class="mx-auto w-auto" src="https://placehold.co/100x20/png"/>
       <h2 class="mt-6 text-black text-2xl font-semibold text-center">Registrieren</h2>
     </div>
 
     <div class="mt-12 sm:mx-auto sm:w-full sm:max-w-[480px]">
-      <div class="px-6 py-12 shadow sm:rounded-lg sm:px-12">
-        <form novalidate @submit="onSubmit">
-          <FormInput
-              autocomplete="email"
+      <form novalidate @submit="onSubmit">
+        <FormInput
+            autocomplete="email"
+            class="text-sm font-semibold"
+            label="E-Mail"
+            name="email"
+            tabindex="1"
+            type="email"
+        />
+        <div class="mt-6">
+          <FormPassword
+              autocomplete="current-password"
               class="text-sm font-semibold"
-              label="E-Mail"
-              name="email"
-              tabindex="1"
-              type="email"
+              label="Passwort"
+              name="password"
+              tabindex="2"
           />
-          <div class="mt-6">
-            <FormPassword
-                autocomplete="current-password"
-                class="text-sm font-semibold"
-                label="Passwort"
-                name="password"
-                tabindex="2"
-            />
-          </div>
-          <div class="mt-6">
-            <FormPassword
-                autocomplete="current-password"
-                class="text-sm font-semibold"
-                label="Passwort wiederholen"
-                name="password_confirm"
-                tabindex="2"
-            />
-          </div>
-
-          <FormSubmit
-              :block="true"
-              :disabled="!meta.dirty"
-              :is-submitting="isSubmitting"
-              class="mt-12"
-              label="Registrieren"
-              tabindex="3"
+        </div>
+        <div class="mt-6">
+          <FormPassword
+              autocomplete="current-password"
+              class="text-sm font-semibold"
+              label="Passwort wiederholen"
+              name="password_confirm"
+              tabindex="2"
           />
-        </form>
+        </div>
 
-        <div class="mt-12">
-          <div class="text-center">
-            <span class="text-sm font-light leading-snug">Du hast bereits einen Account? </span>
-            <NuxtLink class="text-sm font-semibold" to="/auth">Jetzt Einloggen</NuxtLink>
+        <FormSubmit
+            :block="true"
+            :disabled="!meta.dirty"
+            :is-submitting="isSubmitting"
+            class="mt-12"
+            label="Registrieren"
+            tabindex="3"
+        />
+      </form>
+
+      <div class="mt-12">
+        <div class="text-center">
+          <span class="text-sm font-light leading-snug">Du hast bereits einen Account? </span>
+          <NuxtLink class="text-sm font-semibold" to="/auth">Jetzt Einloggen</NuxtLink>
+        </div>
+      </div>
+
+      <div v-if="enableSso">
+        <div class="relative mt-12">
+          <div aria-hidden="true" class="absolute inset-0 flex items-center">
+            <div class="w-full border-t border-gray-200"></div>
+          </div>
+          <div class="relative flex justify-center text-sm/6 font-medium">
+            <span class="bg-white px-6 text-gray-900">Oder weiter mit</span>
           </div>
         </div>
 
-        <div v-if="enableSso">
-          <div class="relative mt-12">
-            <div aria-hidden="true" class="absolute inset-0 flex items-center">
-              <div class="w-full border-t border-gray-200"></div>
-            </div>
-            <div class="relative flex justify-center text-sm/6 font-medium">
-              <span class="px-6 text-gray-900">Oder weiter mit</span>
-            </div>
-          </div>
+        <div class="mt-6 grid grid-cols-2 gap-4">
+          <button
+              class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset hover:bg-gray-50 ring-primary focus-visible:ring-transparent"
+              @click="signUpViaGoogle()"
+          >
+            <IconGoogle></IconGoogle>
+            <span class="text-sm/6 font-semibold">Google</span>
+          </button>
 
-          <div class="mt-6 grid grid-cols-2 gap-4">
-            <button
-                class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset hover:bg-gray-50 ring-primary focus-visible:ring-transparent"
-                @click="signUpViaGoogle()"
-            >
-              <IconGoogle></IconGoogle>
-              <span class="text-sm/6 font-semibold">Google</span>
-            </button>
-
-            <button
-                class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset hover:bg-gray-50 ring-primary focus-visible:ring-transparent"
-                @click="signUpViaApple()"
-            >
-              <IconApple></IconApple>
-              <span class="text-sm/6 font-semibold">Apple</span>
-            </button>
-          </div>
+          <button
+              class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset hover:bg-gray-50 ring-primary focus-visible:ring-transparent"
+              @click="signUpViaApple()"
+          >
+            <IconApple></IconApple>
+            <span class="text-sm/6 font-semibold">Apple</span>
+          </button>
         </div>
       </div>
     </div>
